97b9234d185ac68c6cf1bba7f839863f3d36946a
Aaron Severs <asevers@gmail.com>
2012-01-29 18:48:38 -0500

    testing build failure 1

--- src/main/java/com/buildcoin/plugins/jenkins/BuildcoinProperty.java
+++ src/main/java/com/buildcoin/plugins/jenkins/BuildcoinProperty.java
@@ -1,27 +1,96 @@
 package com.buildcoin.plugins.jenkins;
 
+import hudson.Extension;
 import hudson.model.JobProperty;
 import hudson.model.AbstractProject;
+import hudson.model.JobPropertyDescriptor;
+
 import com.buildcoin.plugins.jenkins.model.Endpoint;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import net.sf.json.JSONObject;
+
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 
 public class BuildcoinProperty extends JobProperty<AbstractProject<?, ?>> {
 
-	final public List<Endpoint> endpoints;
+	private final boolean buildcoinDisabled;
 
 	@DataBoundConstructor
-	public BuildcoinProperty(List<Endpoint> endpoints) {
-		this.endpoints = endpoints;
+	public BuildcoinProperty(boolean buildcoinDisabled) {
+		this.buildcoinDisabled = buildcoinDisabled;
+	}
+	
+    public boolean isProjectDisabled() {
+        return buildcoinDisabled;
+    }
+    
+    @Override
+    public BuildcoinPropertyDescriptor getDescriptor() {
+        return (BuildcoinPropertyDescriptor)super.getDescriptor();
+    }
+
+	@Extension
+	public static final class BuildcoinPropertyDescriptor extends JobPropertyDescriptor {
+		
+		private String buildcoinKey;
+		
+		private boolean isDebugMode;
+		
+		public String getBuildcoinKey() {
+			return buildcoinKey;
+		}
+		
+		public BuildcoinPropertyDescriptor() {
+			super(BuildcoinProperty.class);
+			load();
+		}
+
+		private List<Endpoint> endpoints = new ArrayList<Endpoint>();
+		
+		public List<Endpoint> getEndpoints() {
+			return endpoints;
+		}
+		
+		public boolean isEnabled() {
+			return !endpoints.isEmpty();
+		}
+		
+		public boolean isEnabledByDefault() {
+			return true;
+		}
+		
+		public boolean isDebugMode() {
+			return isDebugMode;
+		}
+
+		public String getDisplayName() {
+			return "Buildcoin integration plugin";
+		}
+		
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+			buildcoinKey = formData.getString("buildcoinKey");
+			isDebugMode = formData.getBoolean("buildcoinDebugMode");
+			endpoints.clear();
+			if (!buildcoinKey.isEmpty() && !isDebugMode) {
+				Endpoint endpoint = new Endpoint();
+				endpoint.setProtocol(Protocol.HTTP);
+				endpoint.setUrl("http://buildcoin.com/hooks/" + formData.getString("buildcoinKey") + "/jenkins");
+				endpoints.add(endpoint);
+			} else if (!buildcoinKey.isEmpty() && isDebugMode) {
+				Endpoint endpoint = new Endpoint();
+				endpoint.setProtocol(Protocol.HTTP);
+				endpoint.setUrl(formData.getString("buildcoinKey"));
+				endpoints.add(endpoint);
+			}
+			save();
+			return super.configure(req,formData);
+		}
+
 	}
 
-	public List<Endpoint> getEndpoints() {
-		return endpoints;
-	}
-
-	public BuildcoinPropertyDescriptor getDescriptor() {
-		return (BuildcoinPropertyDescriptor) super.getDescriptor();
-	}
 }
--- src/main/java/com/buildcoin/plugins/jenkins/Phase.java
+++ src/main/java/com/buildcoin/plugins/jenkins/Phase.java
@@ -2,26 +2,43 @@ package com.buildcoin.plugins.jenkins;
 
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import com.buildcoin.plugins.jenkins.model.Endpoint;
 
 import java.io.IOException;
 import java.util.List;
 
+import com.buildcoin.plugins.jenkins.BuildcoinProperty.BuildcoinPropertyDescriptor;
+import com.buildcoin.plugins.jenkins.model.Endpoint;
+
 
 public enum Phase {
 	STARTED, COMPLETED, FINISHED;
-
-	@SuppressWarnings({ "rawtypes" })
+fdsasdfds
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void handlePhase(Run run, String status, TaskListener listener) {
-			List<Endpoint> endpoints = BuildcoinPropertyDescriptor.endpoints;
-			if (!endpoints.isEmpty()) {
-				for (Endpoint endpoint : endpoints) {
+		BuildcoinProperty property = (BuildcoinProperty) run.getParent().getProperty(BuildcoinProperty.class);
+		if (property != null) {
+			List<Endpoint> targets = property.getDescriptor().getEndpoints();
+			if (property.getDescriptor().isEnabled() && property.isProjectDisabled() == false) {
+				for (Endpoint target : targets) {
 	                try {
-	                	endpoint.getProtocol().sendNotification(endpoint.getUrl(), run.getParent(), run, this, status);
+	                    target.getProtocol().sendNotification(target.getUrl(), run.getParent(), run, this, status);
 	                } catch (IOException e) {
-	                    e.printStackTrace(listener.error("Failed to notify "+endpoint));
+	                    e.printStackTrace(listener.error("Failed to notify buildcoin"));
 	                }
 	            }
 			}
+			return;
+		}
+		BuildcoinPropertyDescriptor defaultProperty = new BuildcoinPropertyDescriptor();
+		List<Endpoint> targets = defaultProperty.getEndpoints();
+		if (defaultProperty.isEnabled()) {
+			for (Endpoint target : targets) {
+				try {
+                    target.getProtocol().sendNotification(target.getUrl(), run.getParent(), run, this, status);
+                } catch (IOException e) {
+                    e.printStackTrace(listener.error("Failed to notify buildcoin"));
+                }
+            }
+		}
 	}
 }
